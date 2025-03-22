e215b4a86bec77c1d31fb0c87f3601c91dcb78eb
Diego González <diegogmrn@gmail.com>
2025-03-22 00:16:18 -0300

    add experience section

---
+++ src/data/experienceData.js
@@ -0,0 +1,22 @@
+export const experienceData =
+[
+    {
+      "title": "Tesista Desarrollador Full Stack en Hospital San Pablo de Coquimbo",
+      "period": "Agosto 2024 - Diciembre 2024",
+      "description": "Diseñé y desarrollé una plataforma para la gestión de traslados extrahospitalarios, utilizando Vue.js, Nest.js y SQL Server.",
+      "achievements": [
+        "Reduje en más del 50% las horas hombre necesarias para reportabilidad.",
+        "Optimicé los procesos clave de gestión, reduciendo en más del 35% la carga administrativa.",
+        "Disminuí el uso de papel en un 75%, contribuyendo a la sostenibilidad del hospital."
+      ]
+    },
+    {
+      "title": "Practica Profesional Desarrollador Full Stack en Autofact",
+      "period": "Enero 2024 - Marzo 2024",
+      "description": "Formé parte del equipo de desarrollo encargado del desarrollo y mantencion del motor de generación de informes de vehículos de la plataforma.",
+      "achievements": [
+        "Implementé nuevas funcionalidades en el motor de generación de informes.",
+        "Realicé seguimiento de buenas prácticas, ayudando a reducir la deuda técnica y mejorar la calidad del código."
+      ]
+    }
+]
